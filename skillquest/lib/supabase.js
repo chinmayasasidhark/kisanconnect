b1fbfbbe212ae99e73d72a5d470c417557654e52
chinmayasasidhark <chinmayasasidhark@gmail.com>
2026-03-28 22:31:00 +0530

    update

---
+++ skillquest/lib/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  "https://pykikkrkvkrnljofnvpy.supabase.co",
+  "sb_publishable_8ulVZgADl8y7fqbesVWTqw_h7Lnfzx7"
+)
