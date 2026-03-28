fd35186502cd83d485c4507cf4b30525a0740627
chinmayasasidhark <chinmayasasidhark@gmail.com>
2026-03-28 22:40:39 +0530

    update

--- skillquest/app/page.tsx
+++ skillquest/app/page.tsx
@@ -4,12 +4,15 @@ import { supabase } from '../lib/supabase'
 
 export default function Home() {
 
-  async function testConnection() {
-    console.log("Button clicked")
-
+  async function addGoal(goalName: string) {
     const { data, error } = await supabase
-      .from('users')
-      .insert([{ email: 'test@gmail.com' }])
+      .from('goals')
+      .insert([
+        {
+          goal_name: goalName,
+          readiness: 0
+        }
+      ])
 
     console.log(data, error)
   }
@@ -18,8 +21,22 @@ export default function Home() {
     <div style={{ padding: 20 }}>
       <h1>SkillQuest 🚀</h1>
 
-      <button onClick={testConnection}>
-        Test Supabase
+      <h2>Select Your Goal</h2>
+
+      <button onClick={() => addGoal("Software Job")}>
+        Software Job
+      </button>
+
+      <br /><br />
+
+      <button onClick={() => addGoal("Internship")}>
+        Internship
+      </button>
+
+      <br /><br />
+
+      <button onClick={() => addGoal("GATE Exam")}>
+        GATE Exam
       </button>
     </div>
   )
